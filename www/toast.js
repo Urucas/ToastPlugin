8e3fa8463b4959d67145583082c777343230d092
Pamela Prosperi <pamepros@gmail.com>
2014-03-06 12:02:08 -0300

    changing order to check functionality

--- www/toast.js
+++ www/toast.js
@@ -1,8 +1,10 @@
 try{
-    console.log("toast->cargo js");
-    window.toast = function(message, duration, success, error){
-    //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
-    cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );
-}
+    var toast = {
+        alert: function(message, duration, success, error){
+            //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
+            cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );
+        }
+    }
+    module.exports = toast;
 }catch(e){console.log("toast->"+e.message)}
 

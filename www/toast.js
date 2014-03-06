ad4307358a0c61a234781e79e3e64154eadbb89c
Bruno Alassia <vruno.alassia@gmail.com>
2014-03-06 11:34:18 -0300

    add toast to window instance

--- www/toast.js
+++ www/toast.js
@@ -1,4 +1,5 @@
-function toast(message, duration, success, error){
+window.toast = function toast(message, duration, success, error){
     //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
     cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );
 }
+

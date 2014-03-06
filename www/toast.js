b8aecd2ac83c648ef3636eabd21a4e8928df19fb
Pamela Prosperi <pamepros@gmail.com>
2014-03-06 11:49:14 -0300

    typo error

--- www/toast.js
+++ www/toast.js
@@ -1,4 +1,5 @@
 try{
+    console.log("toast->cargo js");
     window.toast = function(message, duration, success, error){
     //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
     cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );

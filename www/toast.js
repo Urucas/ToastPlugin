84a77b68c2899e49c206f59a5f659396e80b834a
Pamela Prosperi <pamepros@gmail.com>
2014-03-06 11:40:48 -0300

    trying another way

--- www/toast.js
+++ www/toast.js
@@ -1,5 +1,7 @@
-window.toast = function toast(message, duration, success, error){
+try{
+    window.toast = function(message, duration, success, error){
     //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
     cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );
 }
+}catch(e){console.log("toast->"+e.message)}
 

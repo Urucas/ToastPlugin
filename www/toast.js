ef65d5f4173824131c2e8a25f215fe475c530717
Bruno Alassia <vruno.alassia@gmail.com>
2014-03-07 10:15:52 -0300

    set default params and some validations

--- www/toast.js
+++ www/toast.js
@@ -1,10 +1,31 @@
 try{
     var toast = {
+        /*
+		 *
+		 * @message: String
+		 * @duration: String ["long" : "short"]
+		 * @success: Function
+		 * @error: Function
+		 *
+		 */
         alert: function(message, duration, success, error){
-            //params (success, error, "ToastPlugin", "alert", [{text:"","duration":"short|long"}])
+			if(message == undefined) {
+				console.log("toast called without message");
+				return;
+			}
+			duration = duration == undefined ? "long" : duration;
+			if(success == undefined && !success instanceof Function) {
+				success = null;
+			}
+			if(error == undefined && !error instanceof Function) {
+				error = null;
+			}
             cordova.exec( success, error, "ToastPlugin", "alert", [{"text":message, "duration": duration}] );
         }
     }
     module.exports = toast;
-}catch(e){console.log("toast->"+e.message)}
+
+}catch(e){
+	console.log("toast->"+e.message)
+}
 
